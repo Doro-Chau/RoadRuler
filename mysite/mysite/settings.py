83c6504cd76c4af2c583c8d2095c92314bef3655
趙熙寧 <dorothychau1316@gmail.com>
2022-07-15 16:36:09 +0800

    return not found instead of error message

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -14,7 +14,7 @@ print ("base path", BASE_DIR)
 SECRET_KEY = 'django-insecure-rh&1th*q(b&a6-a##6z2oy5ru450h3rb7vk#&h_r4%px=_w+lj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # ALLOWED_HOSTS = ['35.84.100.18', '127.0.0.1']
 ALLOWED_HOSTS = ['*']
