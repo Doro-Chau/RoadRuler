b6eeb241b1b7a0b6422108d88272372e2e95750c
EC2 Default User <ec2-user@ip-172-31-15-235.us-west-2.compute.internal>
2022-07-19 02:33:51 +0000

    fix 404 problem

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -16,7 +16,7 @@ print ("base path", BASE_DIR)
 SECRET_KEY = 'django-insecure-rh&1th*q(b&a6-a##6z2oy5ru450h3rb7vk#&h_r4%px=_w+lj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # ALLOWED_HOSTS = ['35.84.100.18', '127.0.0.1']
 ALLOWED_HOSTS = ['*']
@@ -123,10 +123,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
+STATIC_ROOT = '/var/www/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
